d54cde8ac4cf11cc94a0d064158f82bbb210fbe5
kive7791 <kvel1097@gmail.com>
2024-12-14 19:39:39 -0700

    Updated for inventory

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -7,6 +7,8 @@ signal lost_timeout_game
 @onready var line2D: Line2D = $Line2D
 @onready var navigation_region: NavigationRegion2D = $NavigationRegion2D
 @onready var Player: CharacterBody2D = $Player
+@onready var Collectable: Area2D = $Crystal
+
 var new_path: PackedVector2Array = PackedVector2Array()
 
 func _ready() -> void:
@@ -14,6 +16,7 @@ func _ready() -> void:
 	$GameplayTimer.one_shot = true  # Timer will not loop
 	Player.connect("game_started", Callable(self, "_on_level_game_started"))
 	$GameplayTimer.connect("timeout", Callable(self, "_on_gameplay_timer_timeout"))
+
 
 func _input(event: InputEvent) -> void:
 	# TODO Defined area for the player to move
@@ -68,3 +71,10 @@ func _on_gameplay_timer_timeout() -> void:
 func _on_pause_pressed() -> void:
 	print("level_1_screen _on_pause_pressed")
 	emit_signal("pause_game")
+
+func _on_crystal_collectable_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if !Input.is_action_pressed("ui_left_mouseclick"):
+		return
+		
+	Player.is_going_to_interact = true
+	Player.interactable_object = Collectable.get_child(shape_idx)
